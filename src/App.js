c2e6b1308485eff485354854242cdbb18684a246
jake <kenshinhm@naver.com>
2019-04-21 23:22:01 +0900

    Setup and a trip to the past

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import "./App.css"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Button danger/>
+                <Button/>
+            </Fragment>
+        );
+    }
 }
+
+const Button = ({danger}) => (
+    <button className={danger? "button button--danger":'button button--success'}>
+        Hello
+    </button>
+    );
 
 export default App;
